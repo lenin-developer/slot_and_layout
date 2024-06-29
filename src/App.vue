e6a1bba4096c06f8f7f414fdcd9197815d2a359a
lenin-developer <40728531+lenin-developer@users.noreply.github.com>
2024-06-29 11:05:12 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import LayoutMain from './components/LayoutMain.vue';
+import ButtonSlot from './components/ButtonSlot.vue';
+import MyIcon from './components/MyIcon.vue';
+</script>
+
+<template>
+  <div>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+
+  <ButtonSlot> hola un texto </ButtonSlot>
+  <ButtonSlot>
+    <span>soy un span</span>
+    <MyIcon />
+  </ButtonSlot>
+
+  <LayoutMain msg="saludoos">
+    <template #header="headerProps">
+      <h1>header , {{ headerProps.mensaje }}</h1>
+    </template>
+
+    <template #default>
+      <h1>default</h1>
+    </template>
+
+    <template #footer="propsFooter">
+      <h1>footer {{ propsFooter.redes }}</h1>
+    </template>
+  </LayoutMain>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/LayoutMain.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{ msg: string }>();
+
+const textoRedes = ref('siguenos en facebook');
+</script>
+
+<template>
+  <div class="container">
+    <header>
+      <slot name="header" :mensaje="msg"></slot>
+    </header>
+    <main>
+      <slot></slot>
+    </main>
+    <footer>
+      <slot name="footer" :redes="textoRedes"></slot>
+    </footer>
+  </div>
+</template>
+
+<style scoped></style>
